13d5f5f2aea4a93314cc3bff2fa72749de334ba4
hermanak <69890492+hermanak@users.noreply.github.com>
2024-03-20 10:30:56 +0100

    Noted down what changes TODO in the future

--- core/src/no/ntnu/game/Models/Settings.java
+++ core/src/no/ntnu/game/Models/Settings.java
@@ -3,6 +3,9 @@ package no.ntnu.game.Models;
 public class Settings {
     private static volatile Settings instance = null;
 
+    // TODO music and sound effects
+    // TODO add player information
+
     // Assumes we are going to be using the Sound class provided by LibGDX
     // https://libgdx.com/wiki/audio/sound-effects
     private float volume;
