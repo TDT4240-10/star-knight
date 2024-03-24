6ae383e5ea9195af0d2128d2cc63c5d246f069ff
Hermann Andreas Kran <hermanakstud.ntnu.no>
2024-03-24 16:49:58 +0100

    GameController
    notify had to be changed to endNotification so that it wouldn't clash with 'notify()' in 'java.lang.Object' going forward

---
+++ core/src/no/ntnu/game/Controllers/GameController.java
@@ -0,0 +1,28 @@
+package no.ntnu.game.Controllers;
+
+public class GameController {
+    int currentLocalPlayerScore;
+    int currentRemotePlayerScore;
+
+    // When the game finish
+    // Using notify like we planned would clash with a 'notify()' in 'java.lang.Object'
+    private void endNotification() {
+        // TODO
+    }
+
+    public void createRoom() {
+        // TODO
+    }
+
+    public void joinRoom(int room_id) {
+        // TODO
+    }
+
+    public void addSubscriber() {
+        // TODO
+    }
+
+    public void removeSubscriber() {
+        // TODO
+    }
+}
