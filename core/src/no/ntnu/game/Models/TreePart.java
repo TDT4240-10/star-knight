c37c51463b892fe6da54f39254877d4574568e1e
Jeff Tan <77041483+Arcyonix@users.noreply.github.com>
2024-03-25 07:09:34 +0100

    Update TreePart.java

--- core/src/no/ntnu/game/Models/TreePart.java
+++ core/src/no/ntnu/game/Models/TreePart.java
@@ -1,2 +1,10 @@
 package no.ntnu.game.Models;public class TreePart {
+        String value;
+        Color color;
+
+        public TreePart(String value, Color color) {
+            this.value = value;
+            this.color = color;
+        }
+    }
 }
