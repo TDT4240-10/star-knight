8750c5e222c9946a15405488d5e4fa723df72113
Hermann Andreas Kran <hermanakstud.ntnu.no>
2024-03-30 19:28:12 +0100

    Added right and left button to the GameScreen
    It is unclear how the internal logic should work together with the tree

--- core/src/no/ntnu/game/Views/GameScreen.java
+++ core/src/no/ntnu/game/Views/GameScreen.java
@@ -1,7 +1,13 @@
 package no.ntnu.game.Views;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
+import no.ntnu.game.Button.Button;
+import no.ntnu.game.Button.ButtonFactory;
+import no.ntnu.game.Button.ButtonInputListener;
 import no.ntnu.game.Controllers.GameController;
 import no.ntnu.game.Models.TreeWithPowerUp;
 
@@ -9,7 +15,13 @@ public class GameScreen extends Screen {
 
     private GameController gameController;
 
+    private Button leftButton;
+
+    private Button rightButton;
+
     private TreeWithPowerUp tree;
+
+    private ShapeRenderer shapeRenderer;
 
     public GameScreen(GameScreenManager gvm) {
         super(gvm);
@@ -18,6 +30,8 @@ public class GameScreen extends Screen {
 
         tree = new TreeWithPowerUp();
         tree.init();
+
+        shapeRenderer = new ShapeRenderer();
     }
 
     @Override
@@ -32,11 +46,31 @@ public class GameScreen extends Screen {
 
     @Override
     public void render(SpriteBatch sb) {
+
+        leftButton = ButtonFactory.createLeftArrowButton(200,300);
+        rightButton = ButtonFactory.createRightArrowButton(800,300);
+
+        // Create input listeners for buttons
+        ButtonInputListener leftInputListener = new ButtonInputListener(leftButton, gvm);
+        ButtonInputListener rightInputListener = new ButtonInputListener(rightButton, gvm);
+        // Set input processors
+        InputMultiplexer inputMultiplexer = new InputMultiplexer();
+
+        inputMultiplexer.addProcessor(leftInputListener);
+        inputMultiplexer.addProcessor(rightInputListener);
+
+        Gdx.input.setInputProcessor(inputMultiplexer);
+
         tree.draw(sb);
+
+        leftButton.render(shapeRenderer,sb);
+        rightButton.render(shapeRenderer,sb);
+
+        shapeRenderer.end();
     }
 
     @Override
     public void dispose() {
-
+        shapeRenderer.dispose();
     }
 }
